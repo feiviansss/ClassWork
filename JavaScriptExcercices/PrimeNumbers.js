4feb63e07994e0c9bed1e4862621d18ebf317f72
feiviansss <112711567+feiviansss@users.noreply.github.com>
2022-10-08 18:46:22 -0500

    Update PrimeNumbers.js

--- JavaScriptExcercices/PrimeNumbers.js
+++ JavaScriptExcercices/PrimeNumbers.js
@@ -1,7 +1,4 @@
-/*
-    Prime Factorization - Have the user enter a number and find
-    all Prime Factors (if there are any) and display them.
-*/
+
 
 var getPrimeFactors = function (n) {
     "use strict";
@@ -20,25 +17,19 @@ var getPrimeFactors = function (n) {
 
     var i, sequence = [];
 
-    //TODO: Check which numbers are factors of n and if that number also happens to be a prime
-    // Print the number of 2s that divide n
     while (n % 2 == 0) {
         System.out.print(2 + " ");
         n /= 2;
     }
 
-    // n must be odd at this point.  So we can
-    // skip one element (Note i = i +2)
+
     for (i = 3; i <= Math.sqrt(n); i += 2) {
-        // While i divides n, print i and divide n
         while (n % i == 0) {
             System.out.print(i + " ");
             n /= i;
         }
     }
 
-    // This condition is to handle the case whien
-    // n is a prime number greater than 2
     if (n > 2)
         System.out.print(n);
 
