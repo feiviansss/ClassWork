12ff9cd0b3d623c11afd5a05bc9c8b874d03a664
feiviansss <112711567+feiviansss@users.noreply.github.com>
2022-10-09 15:44:11 -0500

    Add files via upload

---
+++ node.js/index.js
@@ -0,0 +1,27 @@
+const fs= require("fs");
+
+fs.copyFileSync("file1.txt", "file2.txt");
+
+fs.readFile("file1_2.txt", (err, data) => {
+
+    if (!err) {
+
+        console.log('data: ' + data);
+
+    } else {
+
+        console.log(err);
+
+    }
+
+});
+
+fs.writeFile('helloworld.txt', 'Hello World!', (err) => {
+
+  if (err) return console.log(err);
+
+  console.log('Hello World > helloworld.txt');
+
+});
+
+
