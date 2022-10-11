9db62517592ebcbc739afa10f1612dd12df1e5d8
feiviansss <112711567+feiviansss@users.noreply.github.com>
2022-10-11 13:37:15 -0500

    Add files via upload

---
+++ node.js/indexExternal.js
@@ -0,0 +1,7 @@
+let sh = require("superheroes");
+let superhero = sh.random();
+console.log("My Superhero: " + superhero);
+
+var sv = require("supervillains");
+var supervillain = sv.random();
+console.log("The Supervillain " + supervillain + " is " + superhero + "'s archnemesis");
